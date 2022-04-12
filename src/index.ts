04edcdb20553e5567a5b64a13ae0bd9001394b20
kev <thehailang@gmail.com>
2022-04-12 16:08:23 +0800

    :Tue Apr 12 08:08:23 UTC 2022

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './chain/utility/hash';
 export * from './chain/utility/hash_block';
 export * from './chain/utility/serialize';
 export * from './chain/utility/verify';
+export * from './type';
---
+++ src/type/index.ts
@@ -0,0 +1,3 @@
+export * from './type';
+export * from './block';
+export * from './enum';
